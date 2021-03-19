db3b3ca7fbd578ce119bc0c567a591758cf4c122
Diego Bravo <diegobravo8702@gmail.com>
2021-03-18 22:36:37 -0500

    force deploy on wildfly

---
+++ exchange/deploy-force.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+JBOSS_HOME=/opt/jboss/wildfly
+JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
+JBOSS_MODE=${1:-"standalone"}
+JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
+
+cat /tmp/exchange/batch-deploy.cli.back
+
+echo "==> Executing..."
+#$JBOSS_CLI -c --file=`dirname "$0"`/deploy.cli  --connect
+
+$JBOSS_CLI -c --file=/tmp/exchange/batch-deploy.cli.back  --connect
+
+
